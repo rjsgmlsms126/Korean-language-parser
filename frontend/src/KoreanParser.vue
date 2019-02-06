70ee068f354b1fcce49fb523be9e029f7bfaf415
John Wainwright <johnw3d@gmail.com>
2019-02-06 07:55:33 -0800

    add gtihub link

--- frontend/src/KoreanParser.vue
+++ frontend/src/KoreanParser.vue
@@ -8,7 +8,7 @@
                 <div id="input-title" >Korean sentence parser</div>
                 <div id="attribution">v0.8.0 - <a href="mailto:johnw3d@gmail.com">JBW</a>
                     - based on the <a href="https://github.com/kakao/khaiii">Kakao Hangul Analyzer III</a>
-                    and <a href="https://github.com/johnw3d/Korean-language-parser">JBW's phrase parser</a></div>
+                    and <a href="https://github.com/johnw3d/Korean-language-parser">JBW's phrase parser</a></div> 
             </div>
             <!-- input row -->
             <div class="k-flexrow">
