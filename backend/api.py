5e05bb37a2069250b37eb759b02d4f93be40a488
John Wainwright <johnw3d@gmail.com>
2019-02-07 17:16:37 -0800

    fixed another Khaiii tagging error

--- backend/api.py
+++ backend/api.py
@@ -455,8 +455,6 @@ multiple-clause examples (아/어서, ~면, ...)
 병아리나 물고기도 키워 본 적 없어요?  - gets 키워 본 intermixed wrongly
 
 Fails:
-요즘에는 장르가 다양해졌어요  -  doesn't like the topic marker after the opening adverbial phrase, also the 지 aux verb should be marked as a 'with-the-characteristics-of suffix'
-저는 주로 주인공을 보고 선택해요.  - the auxiliary connector should be part of the main (first) verb(??)   Actually, this is probably OK
 중국 음식을 좋아하기 - not a complete sentence, but declared ok by grammar checker,  note the 을 makes it more than one phrase, but accepting more overrides sentence
 나는 일하러 달려갈 것이다. 그렇지 않으면 나는 거기에 차를 몰고 갈 것이다.   - the "그렇지 않으면" is just "Or,"
 추우면 못 뛰니까 안 뛰겠다.  - two conditionals in one sentene
@@ -465,9 +463,16 @@ Fails:
   교수님은 나에게 “무엇을 일하든지 열심히 일하는 것은 제일 중요하잖아”라고 가르쳐 주셨어요.
 나는 뭐, 심각한 일이라고.  - comma should be in first phrase
 저는 한국어를 진지하게 공부하고 있어요. -  게 not properly attached to 진지하
+
+fixed, but notewworthy:
+저는 주로 주인공을 보고 선택해요.  - the auxiliary connector should be part of the main (first) verb(??)   Actually, this is probably OK
+
   
 To try:
 가방을 산다고 말했어요?
 지원이가 한국에서 여행하러 가겠다고 그랬어요
+
+Khaiii erors:
+여기는 청소년들이 오는 데가 아니에요. - Khaiii calls the first 는 a verb-to-ajective particle!!
 
 """
